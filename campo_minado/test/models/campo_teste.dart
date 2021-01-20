350ed407522d3378763da98078e3ddd68acfa0fc
jamgf <46873184+jamgf@users.noreply.github.com>
2021-01-20 15:34:58 -0300

    first commit

---
+++ campo_minado/test/models/campo_teste.dart
@@ -0,0 +1,39 @@
+import 'package:campo_minado/models/campo.dart';
+import 'package:flutter_test/flutter_test.dart';
+/*
+main() {
+  group('Campo', () {
+    test('Abrir Campo COM explosão', () {
+      Campo c = Campo(0, 0);
+      c.minar();
+
+      expect(c.abrir, throwsException);
+    });
+    test('Abrir Campo SEM explosão', () {
+      Campo c = Campo(0, 0);
+      c.abrir();
+
+      expect(c.aberto, isTrue);
+    });
+    test('Adicionar NÃO Vizinho', () {
+      Campo c1 = Campo(0, 0);
+      Campo c2 = Campo(1, 3);
+      c1.adicionarVizinho(c2);
+
+      expect(c1.vizinhos.isEmpty, isTrue);
+    });
+    test('Adicionar Vizinho', () {
+      Campo c1 = Campo(3, 3);
+      Campo c2 = Campo(3, 4);
+      Campo c3 = Campo(2, 2);
+      Campo c4 = Campo(4, 4);
+
+      c1.adicionarVizinho(c2);
+      c1.adicionarVizinho(c3);
+      c1.adicionarVizinho(c4);
+
+      expect(c1.vizinhos.length, 3);
+    });
+  });
+}
+*/
